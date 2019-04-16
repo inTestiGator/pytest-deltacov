cb56a825851f2b8aefe8c986e361be4102ab3a0f
Andrew Everitt <everitta@allegheny.edu>
2019-04-16 15:59:23 -0400

    Added more to samples

--- sample/src/sample.py
+++ sample/src/sample.py
@@ -1,1 +1,7 @@
 """A sample Python file for the purpose of testing deltacov"""
+
+def addition(a, b):
+    return value
+
+def fill_list():
+    return filled
--- sample/test/test_sample.py
+++ sample/test/test_sample.py
@@ -1,1 +1,7 @@
 """Tests for sample.py"""
+
+from src import sample
+
+def test_addition():
+
+def test_fill_list():
