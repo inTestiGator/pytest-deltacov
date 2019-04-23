8920d72117c028f6a44b24e9cbd4288e9ee1ceca
Andrew Everitt <everitta@allegheny.edu>
2019-04-23 16:01:40 -0400

    Merged zach's work

--- pytest_deltacov.py
+++ pytest_deltacov.py
@@ -1,15 +1,30 @@
 """Deltacov plugin for pytest.
-
 Visually displays changes in code coverage in a graph and indicates changes that
 result in decreased coverage
 """
 
 import pytest
 import argparse
+import pkg_resources
+from pkg_resources import DistributionNotFound, VersionConflict
+from pytest import Collector
+
+__version__= '1.0.0'
+
+dependencies = [
+    'pytest-cov>=2.6.0'
+]
+
+# pytest-cov
+try:
+    pkg_resources.require(dependencies)
+except DistributionNotFound:
+    print("pytest-deltacov requires the package pytest-cov")
+
 
 def pytest_addoption(parser):
-    """ Creates the command line argument for the deltacov plugin """
-    group = parser.getgroup('deltacov')
+    """Collect coverage delta with --delta flag"""
+    group = parser.getgroup('deltacov', 'Coverage deltas')
     group.addoption(
         '--delta',
         action='store',
@@ -17,14 +32,44 @@ def pytest_addoption(parser):
          --delta : Display a graph of code coverage over time (previous ten runs)'
     )
 
+def run_subprocess():
+    """ Runs a subprocess to create the xml file """
+    cmd = "pipenv run pytest -x -s --cov-config pytest.cov --cov-report xml:cov.xml --cov"
+    run.subprocess(cmd.split(), capture_output=False, shell=False)
+
+
+# def parse_xml_for_coverage():
+#     """ Parses the generated xml file """
+#     xml_file = minidom.parse('cov.xml')
+#     all_lines = xml_file.getElementsByTagName('line')
+#
+#     for line in all_lines:
+#         if line.attributes['hits'].value == 0:
+#             line_number = line.attributes['number'].value
+#             #return uncovered lines
+#         else:
+#             #lines are covered
+
 @pytest.fixture
-def bar(request):
-    """Filler docstring, UPDATEME"""
-    return request.config.option.dest_foo
+def empty_cache(request):
+    """Checks cache for covered and uncovered lines"""
+    cov = request.config.cache.get("output/covered", None)
+    uncov = request.config.cache.get("output/uncovered", None)
+    if cov and uncov = None:
+        print("First test execution")
+    return 1
 
 
-def format_csv_file():
-    """Retrieves coverage data and organizes it into a CSV file"""
+def set_cache(cov_list, uncov_list):
+    """Caches covered and uncovered lines for use between runs"""
+    for line in cov_list:
+        config.cache.set("output/covered", line)
+    for line in uncov_list:
+        config.cache.set("output/uncovered", line)
+
+
+def pytest_report_header(config):
+    return "Using --delta coverage report"
 
 
 def graphing_data():
