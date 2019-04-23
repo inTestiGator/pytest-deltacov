8f907fee6fb649bc42d3ad8913ce6a0c6628af40
Zachary Leonardo <leonardoz@aldenv169.allegheny.edu>
2019-04-23 15:42:38 -0400

    Added caching for covered/uncovered lines

--- pytest_deltacov.py
+++ pytest_deltacov.py
@@ -40,17 +40,34 @@ def run_subprocess():
     run.subprocess(cmd.split(), capture_output=False, shell=False)
 
 
-def parse_xml_for_coverage():
-    """ Parses the generated xml file """
-    xml_file = minidom.parse('cov.xml')
-    all_lines = xml_file.getElementsByTagName('line')
+# def parse_xml_for_coverage():
+#     """ Parses the generated xml file """
+#     xml_file = minidom.parse('cov.xml')
+#     all_lines = xml_file.getElementsByTagName('line')
+#
+#     for line in all_lines:
+#         if line.attributes['hits'].value == 0:
+#             line_number = line.attributes['number'].value
+#             #return uncovered lines
+#         else:
+#             #lines are covered
 
-    for line in all_lines:
-        if line.attributes['hits'].value == 0:
-            line_number = line.attributes['number'].value
-            #these are the uncovered lines
-        else:
-            #lines are covered
+@pytest.fixture
+def get_cache(request):
+    """Checks cache for covered and uncovered lines"""
+    cov = request.config.cache.get("output/covered", None)
+    uncov = request.config.cache.get("output/uncovered", None)
+    if cov and uncov = None:
+        print("First test execution")
+    return cov, uncov
+
+
+def set_cache(cov_list, uncov_list):
+    """Caches covered and uncovered lines for use between runs"""
+    for line in cov_list:
+        config.cache.set("output/covered", line)
+    for line in uncov_list:
+        config.cache.set("output/uncovered", line)
 
 
 def pytest_report_header(config):
